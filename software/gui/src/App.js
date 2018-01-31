30f989cda6e3be669bee0fd6cdb2b467b64ecbe4
Luis <yanes.luis@gmail.com>
2018-01-31 18:00:38 +0000

    Name the dials and allow for values to be places in the centre

--- software/gui/src/App.js
+++ software/gui/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ArcChart from './ArcChart'
-import TemperatureDial from './components/TemperatureDial'
+import DialControl from './components/DialControl'
 
 class App extends Component {
   render() {
@@ -18,7 +18,7 @@ class App extends Component {
         <div class="row">
   <div class="col-sm-4">
   <svg 
-          height="200" width="200"><TemperatureDial x={100} y={100} outerRadius={100} innerRadius={50} 
+          height="200" width="200"><DialControl currentValue="Temperature" x={100} y={100} outerRadius={100} innerRadius={50} 
           data={
             [
               {value: 10, label: '-2 C'},
@@ -31,12 +31,12 @@ class App extends Component {
   </div>
   <div class="col-sm-4">
   <svg 
-          height="200" width="200"><TemperatureDial x={100} y={100} outerRadius={100} innerRadius={50} 
+          height="200" width="200"><DialControl currentValue="Illumination" x={100} y={100} outerRadius={100} innerRadius={50} 
           data={
             [
-              {value: 10, label: '-2 C'},
-              {value: 30, label: '16 C'},
-              {value: 80, label: '30 C'}
+              {value: 30, label: '5 lum'},
+              {value: 90, label: '100 lum'},
+              {value: 100, label: '20 lum'}
             ]
             }
           />
@@ -44,7 +44,7 @@ class App extends Component {
 </div>
   <div class="col-sm-4">
   <svg 
-          height="200" width="200"><TemperatureDial x={100} y={100} outerRadius={100} innerRadius={50} 
+          height="200" width="200"><DialControl currentValue="Humidity" x={100} y={100} outerRadius={100} innerRadius={50}
           data={
             [
               {value: 10, label: '-2 C'},
@@ -67,7 +67,7 @@ class App extends Component {
         <input type="text"></input>
         </label>
         <label>
-          Temperature:
+          Value:
         <input type="text"></input>
         </label>
         <button>Submit</button>

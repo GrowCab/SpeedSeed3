14b60e295cada254f32bfc41b4487562fafd3f48
Luis <yanes.luis@gmail.com>
2018-01-31 18:03:01 +0000

    Renamed the temperature component to dial control and refreshed the example.png image

---
+++ software/gui/src/components/DialControl/Arc.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+import {arc} from 'd3'
+
+class Arc extends Component {
+  constructor() {
+    super();
+    this.arc = arc();  }
+
+  componentWillMount() {
+      this.updateD3(this.props);
+  }1
+
+  componentWillReceiveProps(newProps) {
+      this.updateD3(newProps);
+  }
+
+  updateD3(newProps) {
+    this.arc.innerRadius(newProps.innerRadius);
+    this.arc.outerRadius(newProps.outerRadius);
+    }
+
+  render() {
+    return (
+      <path d={this.arc(this.props.data)}
+            style={{fill: this.props.color}}></path>
+  );
+  }
+}
+
+class DialSetting extends Arc {
+  render() {
+      let [labelX, labelY] = this.arc.centroid(this.props.data),
+          labelTranslate = `translate(${labelX}, ${labelY})`;
+
+      return (
+          <g>
+              {super.render()}
+              <text transform={labelTranslate}
+                    textAnchor="middle">
+                  {this.props.data.data.label}
+              </text>
+          </g>
+      );
+  }
+}
+
+export default DialSetting ;
---
+++ software/gui/src/components/DialControl/index.jsx
@@ -0,0 +1,37 @@
+// ./src/components/Piechart/index.jsx
+ 
+import React, { Component } from 'react';
+import { pie } from 'd3';
+import * as scale  from 'd3-scale';
+import DialSetting  from './Arc';
+ 
+class DialControl extends Component {
+  constructor() {
+    super();
+    this.pie = pie().value((d) => d.value);
+    this.colors = scale.scaleOrdinal(scale.schemeCategory20);
+  }
+  arcGenerator(d, i) {
+    return (
+      <DialSetting key={`arc-${i}`}
+        data={d}
+        innerRadius={this.props.innerRadius}
+        outerRadius={this.props.outerRadius}
+        color={this.colors(i)} />
+    );
+  }
+
+  render() {
+    let pie = this.pie(this.props.data),
+      translate = `translate(${this.props.x}, ${this.props.y})`;
+
+    return (
+      <g transform={translate}>
+        {pie.map((d, i) => this.arcGenerator(d, i))}
+        <text textAnchor="middle">{this.props.currentValue}</text>
+      </g>
+    )
+  }
+}
+ 
+export default DialControl;

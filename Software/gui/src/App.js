306d592bcee25cc2c925b446e367553e51d1e2af
Luis Yanes <yanes.luis@gmail.com>
2018-02-24 19:02:27 +0000

    Remove silly moving icon

--- Software/gui/src/App.js
+++ Software/gui/src/App.js
@@ -39,7 +39,6 @@ class App extends Component {
 		return (
 			<div className="App">
 				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
 					<h1 className="App-title">Welcome to SpeedSeed3</h1>
 				</header>
 				<p className="App-intro"></p>

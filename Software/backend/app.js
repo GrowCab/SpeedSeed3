2ce5b297804ca10e9600db953f57908e5c881628
Luis Yanes <yanes.luis@gmail.com>
2018-02-24 20:19:38 +0000

    setSettings interface to store in mongodb

--- Software/backend/app.js
+++ Software/backend/app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var getMonitors = require('./routes/getMonitors');
-var setLuminance = require('./routes/setLuminance');
+var setSettings = require('./routes/setSettings');
 
 var app = express();
 
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/getMonitors', getMonitors);
-app.use('/setLuminance', setLuminance);
+app.use('/setSettings', setSettings);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

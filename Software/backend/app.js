6ef553f30faa662acab993c4aeaea1f2307ff4ae
Ethan Willis <elwillis1@gmail.com>
2018-07-19 10:10:01 -0500

    - Created dockerfile for backend that takes in arguments for environment variables for PORT, DATABASE connection strings, and NODE_ENV. - Created docker compose file for dev in the root directory of the project that can startup the backend and a mongodb instance. - Modified app.js to use our mongoose.js connection code. - Updated project .gitignore so that it ignores a data directory that mongo creates to store all of our database data.

--- Software/backend/app.js
+++ Software/backend/app.js
@@ -1,19 +1,22 @@
 #!/usr/bin/env node
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+//var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var debug = require('debug')('backend:server');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+//var index = require('./routes/index');
+//var users = require('./routes/users');
 var getMonitors = require('./routes/getMonitors');
 var setSettings = require('./routes/setSettings');
 var getSettings = require('./routes/getSettings');
 
 var app = express();
+
+// Add in mongoose and connect to database.
+require('./mongoose')(app);
 
 /**
  * Normalize a port into a number, string, or false.
@@ -59,24 +62,25 @@ function onError(error) {
   }
 }
 
+
 app.on('mongo_ready', function() {
   // view engine setup
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'jade');
+  //app.set('views', path.join(__dirname, 'views'));
+  //app.set('view engine', 'jade');
 
   // uncomment after placing your favicon in /public
   //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
   app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, 'public')));
+  //app.use(cookieParser());
+  //app.use(express.static(path.join(__dirname, 'public')));
 
-  app.use('/', index);
-  app.use('/users', users);
-  app.use('/getMonitors', getMonitors);
-  app.use('/setSettings', setSettings);
-  app.use('/getSettings', getSettings);
+  //app.use('/', index);
+  //app.use('/users', users);
+  //app.use('/getMonitors', getMonitors);
+  //app.use('/setSettings', setSettings);
+  //app.use('/getSettings', getSettings);
 
   // catch 404 and forward to error handler
   app.use(function(req, res, next) {

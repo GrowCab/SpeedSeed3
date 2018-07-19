024bc1d9cf144cbfbf5c7d40fa6b882245a38617
Ethan Willis <elwillis1@gmail.com>
2018-07-19 11:36:11 -0500

    Removed un-necessary fluff from app.js and added in some needed middlewares for performance/functionality

--- Software/backend/app.js
+++ Software/backend/app.js
@@ -1,19 +1,13 @@
 #!/usr/bin/env node
-var express = require('express');
-var path = require('path');
-//var favicon = require('serve-favicon');
-var logger = require('morgan');
-//var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var debug = require('debug')('backend:server');
+let express = require('express');
+let cors = require('cors');
+let compression = require('compression');
+let bodyParser = require('body-parser');
+let logger = require('morgan');
+let debug = require('debug')('backend:server');
 
-//var index = require('./routes/index');
-//var users = require('./routes/users');
-var getMonitors = require('./routes/getMonitors');
-var setSettings = require('./routes/setSettings');
-var getSettings = require('./routes/getSettings');
-
-var app = express();
+// Create new express app.
+let app = express();
 
 // Add in mongoose and connect to database.
 require('./mongoose')(app);
@@ -21,7 +15,6 @@ require('./mongoose')(app);
 /**
  * Normalize a port into a number, string, or false.
  */
-
 function normalizePort(val) {
   var port = parseInt(val, 10);
 
@@ -62,44 +55,16 @@ function onError(error) {
   }
 }
 
-
+// When database in docker container is ready for connections...
 app.on('mongo_ready', function() {
-  // view engine setup
-  //app.set('views', path.join(__dirname, 'views'));
-  //app.set('view engine', 'jade');
-
-  // uncomment after placing your favicon in /public
-  //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+  console.log('Database ready, loading middleware...');
   app.use(logger('dev'));
+  app.use(cors());
+  app.use(compression());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
-  //app.use(cookieParser());
-  //app.use(express.static(path.join(__dirname, 'public')));
 
-  //app.use('/', index);
-  //app.use('/users', users);
-  //app.use('/getMonitors', getMonitors);
-  //app.use('/setSettings', setSettings);
-  //app.use('/getSettings', getSettings);
-
-  // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
-
-  // error handler
-  app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
-
+  // Start app
   var port = normalizePort(process.env.PORT || '3000');
   app.listen(process.env.PORT);
   console.log("Listening on: " + process.env.PORT)

9d721a8ea97d8bdf86ab4211396f8c4dcac8dcf0
Ethan Willis <elwillis1@gmail.com>
2018-07-19 12:24:17 -0500

    Added in loading of schemas for express app.
    Added in loading of API endpoints for express app.

--- Software/backend/app.js
+++ Software/backend/app.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 let express = require('express');
-let cors = require('cors');
-let compression = require('compression');
+//let cors = require('cors');
+//let compression = require('compression');
 let bodyParser = require('body-parser');
 let logger = require('morgan');
 let debug = require('debug')('backend:server');
@@ -57,12 +57,24 @@ function onError(error) {
 
 // When database in docker container is ready for connections...
 app.on('mongo_ready', function() {
+  // Load in express app middleware.
   console.log('Database ready, loading middleware...');
   app.use(logger('dev'));
-  app.use(cors());
-  app.use(compression());
+  //app.use(cors());
+  //app.use(compression());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
+
+  // Pull in schemas
+  console.log("Loading schemas...");
+  require('./schemas/Errors')(app);
+  require('./schemas/Settings')(app);
+  require('./schemas/Status')(app);
+
+  // Start API endpoints
+  console.log("Loading API endpoints...");
+  require('./routes/Settings')(app);
+  require('./routes/Status')(app);
 
   // Start app
   var port = normalizePort(process.env.PORT || '3000');
